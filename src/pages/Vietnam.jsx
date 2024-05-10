7360f455fcb926766f30b21018e621acfec41a35
loeybho <byo_l@naver.com>
2024-05-11 01:32:27 +0900

    Style. 베트남, 몽골 사진 추가

--- src/pages/Vietnam.jsx
+++ src/pages/Vietnam.jsx
@@ -1,7 +1,58 @@
+import van_1 from "@assets/vietnam/viet_1.jpg";
+import van_2 from "@assets/vietnam/viet_2.jpg";
+import van_3 from "@assets/vietnam/viet_3.jpg";
+import van_4 from "@assets/vietnam/viet_4.jpg";
+import van_5 from "@assets/vietnam/viet_5.jpg";
+import van_6 from "@assets/vietnam/viet_6.jpg";
+import { useState } from "react";
+
+const images = [
+  { src: van_1, alt: "밴쿠버 이미지 1" },
+  { src: van_2, alt: "밴쿠버 이미지 2" },
+  { src: van_3, alt: "밴쿠버 이미지 3" },
+  { src: van_4, alt: "밴쿠버 이미지 4" },
+  { src: van_5, alt: "밴쿠버 이미지 5" },
+  { src: van_6, alt: "밴쿠버 이미지 6" },
+];
+
 function Vietnam() {
+  const [modalImage, setModalImage] = useState(null);
+
+  const openModal = (imageSrc) => {
+    setModalImage(imageSrc);
+  };
+
+  const closeModal = () => {
+    setModalImage(null);
+  };
+
   return (
     <>
-      <h1>Vietnam</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image, index) => (
+          <div key={index} className="col-span-3 md:col-span-1">
+            <img
+              src={image.src}
+              alt={image.alt}
+              onClick={() => openModal(image.src)}
+              className="hover:scale-105 transition-transform duration-300 cursor-pointer"
+            />
+          </div>
+        ))}
+      </div>
+
+      {modalImage && (
+        <div
+          className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center bg-white bg-opacity-100 pt-20 cursor-pointer"
+          onClick={closeModal}
+        >
+          <img
+            src={modalImage}
+            alt="모달 이미지"
+            className="max-w-full max-h-full"
+          />
+        </div>
+      )}
     </>
   );
 }
