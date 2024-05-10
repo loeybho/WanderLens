0acd7f871404916b6c649fc38a2860cf886452f9
loeybho <byo_l@naver.com>
2024-05-10 13:49:34 +0900

    Style. title에 카메라 이미지 추가

--- src/components/NavigationMenu/Header.jsx
+++ src/components/NavigationMenu/Header.jsx
@@ -8,15 +8,15 @@ function Header() {
   return (
     <header className="sticky top-0 z-10 border-b-[1px] border-solid border-neutral-100 bg-white/75 py-6">
       <nav className="flex max-w-[800px] mx-auto px-4">
-        <Link to="/" className="flex-grow">
-          <h3
-            className="font-bold font-mansalva text-lg"
+        <h3 className="flex-grow inline-block font-bold font-mansalva text-lg">
+          <Link
+            to="/"
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
           >
-            Wander Lens {isHovered ? "📸" : "📷"}
-          </h3>
-        </Link>
+            Wander Lens &nbsp; {isHovered ? "📸" : "📷"}
+          </Link>
+        </h3>
 
         <span className="hidden md:flex gap-4 text-sm font-semibold">
           <CustomNavLink to="/vancouver">Vancouver</CustomNavLink>
